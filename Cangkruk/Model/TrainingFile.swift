d89f61af19666ff5a054fee023d6b50a2cacb723
Ivone Liwang <liwangivone@gmail.com>
2026-07-09 23:25:28 +0700

    feat: create manager view UI and implement swift data

---
+++ Cangkruk/Model/TrainingFile.swift
@@ -0,0 +1,31 @@
+//
+//  TrainingFile.swift
+//  Cangkruk
+//
+//  Created by Ivone Liwang on 09/07/26.
+//
+
+import Foundation
+import SwiftData
+
+enum TrainingFileSection: String, Codable {
+    case sop
+    case resep
+}
+
+@Model
+final class TrainingFile {
+    var id: UUID
+    var name: String
+    var date: Date
+    var section: String
+    var storedFileName: String
+
+    init(name: String, date: Date = .now, section: TrainingFileSection, storedFileName: String) {
+        self.id = UUID()
+        self.name = name
+        self.date = date
+        self.section = section.rawValue
+        self.storedFileName = storedFileName
+    }
+}
