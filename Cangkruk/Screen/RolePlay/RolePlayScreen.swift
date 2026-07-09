c375bbc77258d77ac14ede923865eefc506408ed
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-07-09 16:55:47 +0700

    feat(home): implementing level screen that has use componentized
    -- What
    
    Implementing the level screen and the applevel component for the list that support the lock & unlock based on the manager props
    
    [#73]

---
+++ Cangkruk/Screen/RolePlay/RolePlayScreen.swift
@@ -0,0 +1,20 @@
+//
+//  RolePlayScreen.swift
+//  Cangkruk
+//
+//  Created by Bee Wijaya on 09/07/26.
+//
+
+import SwiftUI
+
+
+struct RolePlayScreen: View {
+    var body: some View {
+        Text("Role Play")
+    }
+}
+
+
+#Preview {
+    RolePlayScreen()
+}
