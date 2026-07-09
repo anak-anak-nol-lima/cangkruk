5313badeb85c4402b95be89deeeffacb893e8beb
Ivone Liwang <liwangivone@gmail.com>
2026-07-09 23:54:11 +0700

    feat: implement navigation to manager view

--- Cangkruk/Screen/Home/HomeScreen.swift
+++ Cangkruk/Screen/Home/HomeScreen.swift
@@ -16,36 +16,51 @@ struct LevelInfo {
 struct HomeScreen: View {
     // MARK: - Environment
     @Environment(RouterViewModel.self) private var router
+    
+    // MARK: - State
+    @State private var levelInfo: [LevelInfo] = [
+        LevelInfo(level: 1, description: "Pengetahuan akan produk", isLock: false),
+        LevelInfo(level: 2, description: "Memahami kebutuhan pelanggan", isLock: false)
+    ]
 
     var body: some View {
-        @Bindable var router = router
+        VStack(alignment: .leading, spacing: 0) {
+            HStack {
+                Text("Tantangan")
+                    .font(.title2)
+                    .bold()
 
-        ZStack {
-            VStack {
-                HStack {
-                    Text("Tantangan")
-                        .font(.title2)
-                        .bold()
+                Spacer()
+
+                Image(systemName: "lock")
+                    .frame(width: 44, height: 44)
+                    .foregroundStyle(.black)
+                    .background(.white)
+                    .overlay {
+                        Circle()
+                            .stroke(.black, lineWidth: 1.5)
+                    }
+                    .clipShape(Circle())
+            }
+            .padding()
+
+            ZStack {
+                VStack {
+                    ForEach(levelInfo.indices, id: \.self) { idx in
+                        let level = levelInfo[idx]
+
+                        AppLevel(level: level.level, description: level.description, isLock: level.isLock, isManager: true
+                        ) {
+                            // locking or unlock the app
+                            levelInfo[idx].isLock.toggle()
+                        } onClick: {
+                            // navigation to next screen
+                            router.push(.level)
+                        }
+                    }
+
 
                     Spacer()
-
-                    Image(systemName: "lock")
-                        .frame(width: 44, height: 44)
-                        .foregroundStyle(.black)
-                        .background(.white)
-                        .overlay {
-                            Circle()
-                                .stroke(.black, lineWidth: 1.5)
-                        }
-                        .clipShape(Circle())
-                        .onTapGesture {
-                            router.push(.register)
-                        }
-                }
-                .padding()
-
-                if !router.isManagerUnlocked {
-                    GuestViewScreen()
                 }
             }
         }
