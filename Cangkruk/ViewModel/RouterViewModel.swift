bdca0b6deade852edc4043372d815c8b58a43c34
Ivone Liwang <liwangivone@gmail.com>
2026-07-12 03:54:07 +0700

    feat: implement navigation between auth screen and home screen

--- Cangkruk/ViewModel/RouterViewModel.swift
+++ Cangkruk/ViewModel/RouterViewModel.swift
@@ -12,24 +12,29 @@ enum Route {
     case level,
          roleplay,
          home,
-         termsConditions
+         termsConditions,
+         register,
+         login
 }
 
 @MainActor
 @Observable
 class RouterViewModel {
     var path = NavigationPath()
-    
+
+    // Set to true once register + login succeed; HomeScreen observes this to reveal ManagerView.
+    var isManagerUnlocked = false
+
     func push(_ p: Route) {
         // pushing the path of next navigation page
         path.append(p)
     }
-    
-    func pop() {
-        // go back to previous page
-        path.removeLast()
+
+    func pop(_ count: Int = 1) {
+        // go back `count` pages
+        path.removeLast(min(count, path.count))
     }
-    
+
     func popToRoot() {
         // jump to first page
         path = NavigationPath()
