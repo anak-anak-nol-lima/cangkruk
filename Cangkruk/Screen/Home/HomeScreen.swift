84adbbd3d33bf668bb63e7f2aea48d2bdeab23a1
Ivone Liwang <liwangivone@gmail.com>
2026-07-12 03:54:07 +0700

    feat: implement navigation between auth screen and home screen

--- Cangkruk/Screen/Home/HomeScreen.swift
+++ Cangkruk/Screen/Home/HomeScreen.swift
@@ -14,54 +14,42 @@ struct LevelInfo {
 }
 
 struct HomeScreen: View {
-    // MARK: - State
-    @State private var levelInfo: [LevelInfo] = [
-        LevelInfo(level: 1, description: "Pengetahuan akan produk", isLock: false),
-        LevelInfo(level: 2, description: "Memahami kebutuhan pelanggan", isLock: false)
-    ]
+    // MARK: - Environment
+    @Environment(RouterViewModel.self) private var router
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            HStack {
-                Text("Tantangan")
-                    .font(.title2)
-                    .bold()
+        @Bindable var router = router
 
-                Spacer()
-
-                Image(systemName: "lock")
-                    .frame(width: 44, height: 44)
-                    .foregroundStyle(.black)
-                    .background(.white)
-                    .overlay {
-                        Circle()
-                            .stroke(.black, lineWidth: 1.5)
-                    }
-                    .clipShape(Circle())
-            }
-            .padding()
-
-            ZStack {
-                VStack {
-                    ForEach(levelInfo.indices, id: \.self) { idx in
-                        let level = levelInfo[idx]
-
-                        AppLevel(level: level.level, description: level.description, isLock: level.isLock, isManager: true
-                        ) {
-                            // locking or unlock the app
-                            levelInfo[idx].isLock.toggle()
-                        } onClick: {
-                            // navigation to next screen
-                            router.push(.level)
-                        }
-                    }
-
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Tantangan")
+                        .font(.title2)
+                        .bold()
 
                     Spacer()
+
+                    Image(systemName: "lock")
+                        .frame(width: 44, height: 44)
+                        .foregroundStyle(.black)
+                        .background(.white)
+                        .overlay {
+                            Circle()
+                                .stroke(.black, lineWidth: 1.5)
+                        }
+                        .clipShape(Circle())
+                        .onTapGesture {
+                            router.push(.register)
+                        }
+                }
+                .padding()
+
+                if !router.isManagerUnlocked {
+                    GuestViewScreen()
                 }
             }
         }
-        .sheet(isPresented: $isManager) {
+        .sheet(isPresented: $router.isManagerUnlocked) {
             ManagerView()
         }
         .navigationBarBackButtonHidden()
