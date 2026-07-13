aca22d4712f8e5cf61bc505dd1aaa5a59b68ffae
Stefanie Agahari <stefanie.agahari@gmail.com>
2026-07-13 15:12:01 +0700

    feat (level): implement real assets to level detail page

---
+++ Cangkruk/FontExtension.swift
@@ -0,0 +1,14 @@
+//
+//  FontExtension.swift
+//  Cangkruk
+//
+//  Created by Stefanie Agahari on 13/07/26.
+//
+
+import SwiftUI
+
+extension Font {
+    static func shakyComicBold(size: CGFloat) -> Font {
+        return .custom("ShakyHandSomeComic-Bold", size: size)
+    }
+}
