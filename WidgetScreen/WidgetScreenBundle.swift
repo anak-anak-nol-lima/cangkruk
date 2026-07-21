3e31b5ed339ef344daaa3e1e2881c7529bcce7ed
Stefanie Agahari <stefanie.agahari@gmail.com>
2026-07-21 16:06:32 +0700

    feat (widget): implement the widget

---
+++ WidgetScreen/WidgetScreenBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetScreenBundle.swift
+//  WidgetScreen
+//
+//  Created by Stefanie Agahari on 20/07/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetScreenBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetScreen()
+        WidgetScreenControl()
+    }
+}
